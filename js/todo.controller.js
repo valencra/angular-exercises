3fa9053074989a9d08b66763924d37ef893e435c
valencra <valencra@mcmaster.ca>
2017-06-27 11:58:53 -0400

    Added sseed files, initial module and controller set-up

---
+++ js/todo.controller.js
@@ -0,0 +1,16 @@
+function TodoController() {
+  this.list = [{
+  	title: 'First todo item!',
+  	completed: false
+  }, {
+  	title: 'Second todo item!',
+  	completed: false
+  }, {
+  	title: 'Third todo item!',
+  	completed: false
+  }];
+}
+
+angular
+  .module('app')
+  .controller('TodoController', TodoController);
